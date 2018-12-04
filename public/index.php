11868fb59c4866c14ccc0591c228d6667263c299
David  LAGEISTE <codfrc@gmail.com>
2018-12-04 17:48:47 +0100

    index

--- public/index.php
+++ public/index.php
@@ -8,6 +8,8 @@ use Symfony\Component\HttpFoundation\Request;
 require __DIR__.'/../vendor/autoload.php';
 
 // The check is to ensure we don't use .env in production
+
+ini_set('memory_limit', '-1');
 if (!isset($_SERVER['APP_ENV']) && !isset($_ENV['APP_ENV'])) {
     if (!class_exists(Dotenv::class)) {
         throw new \RuntimeException('APP_ENV environment variable is not defined. You need to define environment variables for configuration or add "symfony/dotenv" as a Composer dependency to load variables from a .env file.');
